e25bd807fc22656cb7cc067c0bd9fdb64a3cbbed
anastanjohnson <124351526+anastanjohnson@users.noreply.github.com>
2026-08-21 10:32:02 +0200

    Add repeat guest data for 2025 and 2026

--- src/data/repeatGuestData.js
+++ src/data/repeatGuestData.js
@@ -16,3 +16,51 @@ export const repeatGuestData = {
     visitGapStats: { medianDays: 95, meanDays: 130.8, totalReturnVisitsAnalyzed: 1173 },
     totalTrackedGuests: 4627,
   };
+
+export const repeatGuestLists = {
+    "2025": {
+        fiveOrMore: [
+            { name: "Sebastian Ehrenstein", lastVisit: "2025-10-31", totalVisits: 13 },
+            { name: "Steffi  Brüggen", lastVisit: "2025-12-06", totalVisits: 12 },
+            { name: "Kerstin Çebe", lastVisit: "2025-12-01", totalVisits: 10 },
+            { name: "Michael Mailänder", lastVisit: "2025-11-08", totalVisits: 8 },
+            { name: "Klaus-jürgen Wrede", lastVisit: "2025-12-11", totalVisits: 8 },
+            { name: "Sebastian  Hoffmann", lastVisit: "2025-12-27", totalVisits: 7 },
+            { name: "Veer Singh", lastVisit: "2025-12-31", totalVisits: 7 },
+            { name: "Lorena Knoke", lastVisit: "2025-10-11", totalVisits: 6 },
+            { name: "Christiane Mehling", lastVisit: "2025-10-20", totalVisits: 6 },
+            { name: "Sonja  Grützenbach", lastVisit: "2025-11-01", totalVisits: 6 },
+            { name: "Enzo Rosin", lastVisit: "2025-12-26", totalVisits: 6 },
+            { name: "Karolina Bernhardt", lastVisit: "2025-12-28", totalVisits: 6 },
+            { name: "Holger Mengel", lastVisit: "2025-12-28", totalVisits: 6 },
+            { name: "Sabine Gasch", lastVisit: "2025-10-10", totalVisits: 5 },
+            { name: "Sascha Pelkner", lastVisit: "2025-10-18", totalVisits: 5 },
+            { name: "Dominik Braunsteiner", lastVisit: "2025-11-14", totalVisits: 5 },
+            { name: "Teena Hassan", lastVisit: "2025-11-23", totalVisits: 5 },
+            { name: "Andrea Becker", lastVisit: "2025-12-04", totalVisits: 5 },
+            { name: "Yash Jan", lastVisit: "2025-12-13", totalVisits: 5 },
+            { name: "Lidija Lihovic", lastVisit: "2025-12-14", totalVisits: 5 },
+            { name: "Sangetha  Thameskumar", lastVisit: "2025-12-19", totalVisits: 5 },
+            { name: "Katrin  Dohrmann", lastVisit: "2025-12-20", totalVisits: 5 },
+            { name: "Ali Aslan", lastVisit: "2025-12-24", totalVisits: 5 },
+            ],
+        tenOrMore: [
+            { name: "Sebastian Ehrenstein", lastVisit: "2025-10-31", totalVisits: 13 },
+            { name: "Steffi  Brüggen", lastVisit: "2025-12-06", totalVisits: 12 },
+            { name: "Kerstin Çebe", lastVisit: "2025-12-01", totalVisits: 10 },
+            ],
+    },
+    "2026": {
+        fiveOrMore: [
+            { name: "Sebastian  Hoffmann", lastVisit: "2026-08-08", totalVisits: 13 },
+            { name: "Balakarthik Palani", lastVisit: "2026-07-11", totalVisits: 6 },
+            { name: "Pradeep Kumar", lastVisit: "2026-08-15", totalVisits: 6 },
+            { name: "Veer Singh", lastVisit: "2026-06-20", totalVisits: 5 },
+            { name: "Stephy Stani", lastVisit: "2026-07-25", totalVisits: 5 },
+            { name: "Guido Heymann", lastVisit: "2026-08-06", totalVisits: 5 },
+            ],
+        tenOrMore: [
+            { name: "Sebastian  Hoffmann", lastVisit: "2026-08-08", totalVisits: 13 },
+            ],
+    },
+};
